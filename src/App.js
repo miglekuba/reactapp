22ee0e6b0b9b4f02ac069078e7c3b42dd8e07010
Migle Kubiliute <migle.kubiliute@gmail.com>
2020-01-08 16:23:56 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  increaseCounter = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  handleForm = (event) => {
+    event.preventDefault();
+    this.setState({ todos: [...this.state.todos, this.state.todo] })
+  };
+  handleChange = (event) => {
+  this.setState({todo: event.target.value});
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      number: 0,
+      todos: [],
+      todo: ""
+    };
+    
+  }
+
+  render() {
+    const listItems = this.state.todos.map((todo) => <li>{todo}</li>);
+    return (
+      <div>
+        <form onSubmit={this.handleForm}>
+          <label>
+            To do List:
+          <input type="text" name="name" onChange={this.handleChange} />          
+          <ul>{listItems}</ul>  
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <br></br>
+        <button onClick={this.increaseCounter}> CLICK ME</button>
+        <h1>{this.state.number}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
